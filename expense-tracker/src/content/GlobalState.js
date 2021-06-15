cacab6f1db398ee6e436b478f95bba2e40fe13ea
Ian Clark <ianclark226@gmail.com>
2021-06-15 12:38:01 -0400

    add and delete

--- expense-tracker/src/content/GlobalState.js
+++ expense-tracker/src/content/GlobalState.js
@@ -3,14 +3,7 @@ import AppReducer from './AppReducer';
 
 // Initial State
 const initialState = {
-    transactions: [
-        
-    { id: 1, text: 'Flower', amount: -20 },
-  { id: 2, text: 'Salary', amount: 300 },
-  { id: 3, text: 'Book', amount: -10 },
-  { id: 4, text: 'Camera', amount: 150 }
-
-    ]
+    transactions: []
 }
 
 // Create context
@@ -21,9 +14,26 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider= ({ children }) => {
     const [state, dispatch] = useReducer(AppReducer, initialState);
 
+    // Actions to make calls to reduceer
+    function deleteTransaction(id) {
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id,
+        })
+    }
+
+    function addTransaction(transaction) {
+        dispatch({
+            type: 'ADD_TRANSACTION',
+            payload: transaction,
+        })
+    }
+
     return(
         <GlobalContext.Provider value={{
-            transactions: state.transactions
+            transactions: state.transactions,
+            deleteTransaction,
+            addTransaction
         }}>
             {children}
         </GlobalContext.Provider>
